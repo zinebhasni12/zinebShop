90283fe0a9d54e03e50bfcdb49334e84d59bf84f
zinebhasni12 <zinebhasni.2011@gmail.com>
2024-01-10 12:15:30 +0100

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
         <div className="bg-gray-100 p-6 rounded-lg shadow-md mt-20">
             <h2 className="text-2xl font-bold mb-2">À propos de Nous</h2>
             <p className="text-gray-700">
-                Bienvenue sur Zineb shop. Nous sommes dédiés à vous donner le meilleur des   produits, avec un accent sur la fiabilité, la variété des produits, le service client.
+                Bienvenue sur Zineb shop. Nous sommes dédiés à vous donner le meilleur des   produits, avec un accent de  fiabilité, la variété des produits, le service client.
             </p>
             <p className="text-gray-700 mt-4">
                 Fondé Cette année par Zineb Alaoui, Zineb shop a commencé, son enthousiasme pour fournir le meilleur produit avec une qualité  l'a poussé à action qu'il/elle a faite, par exemple 'faire des recherches intensives, quitter son travail quotidien'l'impulsion pour transformer le travail acharné et l'inspiration en un magasin en ligne en plein essor.
