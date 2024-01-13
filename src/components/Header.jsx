a78810ad21c7607b0b23dd2e7a85f80005b57ca1
zinebhasni12 <zinebhasni.2011@gmail.com>
2024-01-13 15:50:17 +0100

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -107,6 +107,16 @@ export const Header = ({toggleDarkMode}) => {
                                 <Link to="/contact"
                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</Link>
                             </li>
+                            <li>
+                                <Link to="/Shopping Cart" 
+                    
+                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Shopping Cart</Link>
+                                </li>
+    
+                            <li>
+
+                            </li>
+                        
 
 
                         </ul>
