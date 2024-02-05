77be6bfce95a49ca6c664037ffca3b935136f376
zinebhasni12 <zinebhasni.2011@gmail.com>
2024-02-05 16:27:10 +0100

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import zinebshop from '../assets/photos/zinebshop.svg'
+import zinebshop from '../assets/photos/zinebshop.png'
 import zinebshopDark from '../assets/photos/zinebshopDark.svg'
 import profile from '../assets/photos/profil.jpg'
 import { Link, useNavigate } from "react-router-dom";
@@ -11,7 +11,7 @@ export const Header = ({ toggleDarkMode }) => {
         <nav className="fixed top-0 left-0 w-full bg-gray-100 border-gray-200 dark:bg-gray-900 border-b dark:border-gray-200 z-50">
             <div className=" flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center space-x-3">
-                    <img src={zinebshop} className="h-12 dark:hidden" alt="Flowbite Logo Light" />
+                    <img src={zinebshop} className='h-12' alt="Flowbite Logo Light" />
                     <img src={zinebshopDark} className="h-12 hidden dark:block" alt="Flowbite Logo Dark" />
                 </a>
 
@@ -92,7 +92,7 @@ export const Header = ({ toggleDarkMode }) => {
 
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                     id="navbar-user">
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
+                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 :border-purple-700">
                         <li>
                             <Link to="/"
                                 className="block py-2 px-3 text-white bg-red-700 rounded md:bg-transparent md:text-red-700 md:p-0 md:dark:text-red-500"
